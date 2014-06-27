5e9c9498d243beb62a6018df53a9f14e4e9e5083
Colin Pierce <cpierce46@gmail.com>
2014-06-27 08:52:58 -0600

    completed guess that number

--- GuessThatNumber/Program.cs
+++ GuessThatNumber/Program.cs
@@ -10,7 +10,37 @@ namespace GuessThatNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Want to play a game?");
+            GuessThatNumber();
             Console.ReadKey();
+        }
+        static void GuessThatNumber()
+        {
+            Console.WriteLine("What number am I thinking of?");
+            Random compGuess = new Random();
+            int randomNum = compGuess.Next(1, 101);
+            int count = 0;
+            int newInput = 0;
+            Console.WriteLine("Take a guess.");
+            while (newInput != randomNum)
+            {
+                string yourGuess = Console.ReadLine();
+                int input = int.Parse(yourGuess);
+                if (input < randomNum)
+                {
+                    Console.WriteLine("Go higher.");
+                    count++;
+                }
+                else if (input > randomNum)
+                {
+                    Console.WriteLine("Go lower.");
+                    count++;
+                }
+                else if (input == randomNum)
+                {
+                    Console.WriteLine("You got it right! You took " + (count + 1) + " guesses. Want to play again?");
+                }
+            }
         }
     }
 }
